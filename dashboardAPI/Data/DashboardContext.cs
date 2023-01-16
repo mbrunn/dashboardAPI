6cd51adee94ed35ad6d7f11d3774eedf1e6ea004
Sims <if21b025@technikum-wien.at>
2023-01-16 17:51:41 +0100

    Initial Commit

---
+++ dashboardAPI/Data/DashboardContext.cs
@@ -0,0 +1,10 @@
+﻿using dashboardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dashboardAPI.Data {
+    public class DashboardContext : DbContext {
+        public DashboardContext(DbContextOptions<DashboardContext> options) : base(options) { }
+
+        public DbSet<UserCredentials> _users { get; set; }
+    }
+}
---
+++ dashboardAPI/Models/UserCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dashboardAPI.Models {
+    public class UserCredentials {
+        [Key]
+        [StringLength(24, ErrorMessage = "UserName cannot exceed more than 24 characters")]
+        [RegularExpression(@"^[A-Za-z0-9]{4,24}$", ErrorMessage = "UserName cannot have special characters or space and must be at least 4 characters long!")]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        
+        public string Email { get; set; }
+    }
+}
